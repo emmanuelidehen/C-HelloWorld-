14052f2bf586f051da00335419bc514daa87659e
Emmanuel idehen <emmanuel.idehen@mvsu.edu>
2019-03-14 17:55:34 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Hello World!
+//
+//  Created by Emmanuel Idehen on 3/14/19.
+//  Copyright © 2019 Emmanuel Idehen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  
+    printf("Hello, World!\n");
+    printf("The funny thing is C is a father to objective C and C++ Lol !");
+    return 0;
+}
